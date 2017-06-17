f99b69e22815625bd12bef3e771d8da75ca36ac7
James Rothpearl <jrothpearl@gmail.com>
2017-06-16 22:45:49 -0400

    first commit

---
+++ utils/ReddcoinHandler.js
@@ -0,0 +1,5 @@
+// https://github.com/bretth18/PresidioWallet/tree/master/App/Bitcoin (bitcoinhandler.js and api.js)
+
+
+// import andy's deps including reddcoin-electrum-js, create wallet factory methods below
+// for use in components
